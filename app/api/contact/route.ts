f90b4c664340b868f8eab9e3b57d854972555a0a
lamaachi <istatasila7@gmail.com>
2025-09-06 06:20:20 +0100

    fix : env var for build

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,10 +3,10 @@ import { ContactTemplate } from "@/emails/ContactTemplate";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
 
+  const resend = new Resend(process.env.RESEND_API_KEY);
   
   if (!process.env.RESEND_API_KEY) {
     return Response.json({ error: 'API key not configured' }, { status: 500 });
